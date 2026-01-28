d7db60408b1e11f6135fd26725e2feb9722f249c
vineethaw <60957551+vineethaw@users.noreply.github.com>
2026-01-28 18:22:37 +0530

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Vineetha Wilson | Senior QA Automation Engineer UAE',
+  description: 'Senior QA Automation Engineer & QA Team Lead in Dubai UAE.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ next.config.js
@@ -0,0 +1,1 @@
+/** @type {import('next').NextConfig} */ const nextConfig = {}; module.exports = nextConfig;
